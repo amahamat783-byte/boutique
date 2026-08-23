6942a17f1adaca3aad53a00b1480f2ab3a2b26e8
mht_abdou10 <amahamat783@gmail.com>
2026-08-23 11:51:02 +0100

    Add files via upload

---
+++ boutique.py
@@ -0,0 +1,37 @@
+prix_Eau=500
+prix_Jus=1000
+prix_Soda=1500
+print("="*30)
+print(f"1 :l'eau est à   :{prix_Eau} FCFA")
+print(f"2 :le Jus est à  :{prix_Jus} FCFA")
+print(f"3 :le Soda est à :{prix_Soda} FCFA")
+print("="*30)
+
+demande=input("Choisissez ce que vous voulez monsieur : 1-Eau ,2-Jus ,3-Soda  :")
+
+if demande=='1':
+    mon_argent=int(input("Entrez votre argent :"))
+    if mon_argent>=prix_Eau:
+        print("Eau distribué")
+        monnaie=mon_argent-prix_Eau
+        print("Solde restant :",monnaie)
+    else:
+        print("Argent insuffisent")
+elif demande=='2':
+    mon_argent=int(input("Entrez votre argent :"))
+    if mon_argent>=prix_Jus:
+        print("Jus distribué")
+        monnaie=mon_argent-prix_Jus
+        print("Solde restant :",monnaie)
+    else:
+        print("Argent insuffisent")
+elif demande=='3':
+    mon_argent=int(input("Entrez votre argent :"))
+    if mon_argent>=prix_Soda:
+        print("Soda distribué")
+        monnaie=mon_argent-prix_Soda
+        print("Solde restant :",monnaie)
+    else:
+        print("Argent insuffisent")
+else:
+    print("Choix invalide")
